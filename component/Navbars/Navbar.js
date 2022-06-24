767f7437d63ba606b9b41f6d3bdf3eee0fd55b70
Monica Mehta <monicamehta0102@gmail.com>
2022-06-24 16:55:51 +0530

    Minor changes

--- component/Navbars/Navbar.js
+++ component/Navbars/Navbar.js
@@ -12,9 +12,7 @@ const HomeNavbar = () => {
                         require("../../assets/images/logoicon.png")
                             .default
                     } />Ipangram</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+                
                 <div className="collapse navbar-collapse pt-2 pb-0" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto mb-lg-0">
                         <li className="nav-item">
